67b7b1f35c364f6256b2403f367165d2ff4ac82b
13604786021 <CIEC_iMac@ciec-imacs-imac.local>
2016-06-23 09:22:16 +0800

    Initial Commit

---
+++ counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  counter
+//
+//  Created by CIEC_iMac on 16/6/23.
+//  Copyright © 2016年 CIEC_iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
