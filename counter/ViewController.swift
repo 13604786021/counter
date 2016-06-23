8bfda4db1f1c588b6a27ce3ae183d164b515eb38
13604786021 <CIEC_iMac@CIEC-iMacs-iMac.local>
2016-06-23 16:50:06 +0800

    计算器
    为数字和运算符按钮添加动作事件，实现简单的四则运算

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -10,7 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var value: UITextField!
+    var s=""
+    @IBAction func one(sender: AnyObject) {
+        s=s+"1"
+        value.text=s
+    }
+    @IBAction func two(sender: AnyObject) {
+        s=s+"2"
+        value.text=s
+    }
+    @IBAction func three(sender: AnyObject) {
+        s=s+"3"
+        value.text=s
+    }
+    @IBAction func four(sender: AnyObject) {
+        s=s+"4"
+        value.text=s
+    }
+    @IBAction func five(sender: AnyObject) {
+        s=s+"5"
+        value.text=s
+    }
+    @IBAction func six(sender: AnyObject) {
+        s=s+"6"
+        value.text=s
+    }
+    @IBAction func seven(sender: AnyObject) {
+        s=s+"7"
+        value.text=s
+    }
+    @IBAction func eight(sender: AnyObject) {
+        s=s+"8"
+        value.text=s
+    }
+    @IBAction func nine(sender: AnyObject) {
+        s=s+"9"
+        value.text=s
+    }
+  
+    @IBAction func zero(sender: AnyObject) {
+        s=s+"0"
+        value.text=s
+    }
+    
+    @IBAction func dot(sender: AnyObject) {
+        s=s+"."
+        value.text=s
+    }
     override func viewDidLoad() {
+       
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
